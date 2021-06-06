cbfefb21dcc62d89cdc8d47c79a1347e245dfcf5
Bregy Malpartida <bregy@minsky.cc>
2021-06-06 01:24:54 -0500

    feat: dependencies optimized for more lightweigth container image

--- database/database.py
+++
@@ -1,9 +0,0 @@
-# import peewee as pw
-
-
-# class Person(pw.Model):
-#     name = pw.CharField()
-#     birthday = pw.DateField()
-
-#     class Meta:
-#         database = db # This model uses the "people.db" database.
